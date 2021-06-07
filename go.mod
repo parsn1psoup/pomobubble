cd147345bcc70bc9ead2b5790b921ed07929c00a
Kathrin H <parsn1psoup@users.noreply.github.com>
2021-06-07 22:24:45 +0200

    Integrate progress bar

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/parsn1psoup/pomobubble
 
 go 1.16
 
-require github.com/charmbracelet/bubbletea v0.14.0 // indirect
+require (
+	github.com/charmbracelet/bubbles v0.8.0 // indirect
+	github.com/charmbracelet/bubbletea v0.14.0 // indirect
+)
--- main.go
+++ main.go
@@ -3,77 +3,128 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 	"time"
 
+	"github.com/charmbracelet/bubbles/progress"
 	tea "github.com/charmbracelet/bubbletea"
 )
 
-// todo
+// TODO
+// start timer on enter
 // add break duration
-// make durations configurable
-// add progress bar
-// add more key controls
+// make durations configurable (with restrictions)
+// add more key controls (e.g. pause timer)
+// track across multiple pomodoros
+// altscreen?
+// how to notify (sound)?
 
+// so far, heavily based on https://github.com/charmbracelet/bubbletea/blob/master/examples/countdown/main.go
+// and https://github.com/charmbracelet/bubbletea/blob/master/examples/progress/main.go
 
-// so far, basically a copy of https://github.com/charmbracelet/bubbletea/blob/master/examples/countdown/main.go
+const durationMinutes = 1
+const duration = time.Minute * durationMinutes
+const interval = time.Second // refresh interval
 
-const duration = time.Minute * 25
-const interval = time.Second
+const (
+	fps              = 60 // why should stepSize depend on fps? is Update() called on every new frame?
+	stepSize float64 = 1.0 / (float64(fps))
+	padding          = 2
+	maxWidth         = 80
+)
 
-type model struct {
+type pomoModel struct {
 	timeout  time.Time
 	lastTick time.Time
+	percent  float64
+	progress *progress.Model
 }
 
 // transmits time from ticker to update function
 type tickMsg time.Time
 
-func (m model) Init() tea.Cmd {
-	fmt.Println("Press Enter to start your first Pomodoro!\nDefault length is 25 minutes.\n")
-	return tick()
-}
-
-func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	switch msg := msg.(type) {
-	case tea.KeyMsg:
-		switch msg.String() {
-		case "ctrl+c", "q", "esc":
-			return m, tea.Quit
-		}
-
-	case tickMsg:
-		t := time.Time(msg)
-		if t.After(m.timeout) {
-			return m, tea.Quit
-		}
-		m.lastTick = t
-		return m, tick()
-	}
-
-	return m, nil
-}
-
-func (m model) View() string {
-	t := m.timeout.Sub(m.lastTick).Round(time.Second)
-	return fmt.Sprintf("This program will quit in %v\n", t)
-}
-
-func main() {
-
-	m := model{
-		timeout:  time.Now().Add(duration),
-		lastTick: time.Now(),
-	}
-
-	if err := tea.NewProgram(m).Start(); err != nil {
-		fmt.Printf("Couldn't start program: %v\n", err)
-		os.Exit(1)
-	}
-
-}
-
 func tick() tea.Cmd {
 	return tea.Tick(time.Duration(interval), func(t time.Time) tea.Msg {
 		return tickMsg(t)
 	})
+}
+
+func (pm pomoModel) Init() tea.Cmd {
+	fmt.Printf("Timer is running! Duration is %v minutes.\n", durationMinutes)
+	return tick()
+}
+
+func (pm pomoModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "q", "esc":
+			return pm, tea.Quit
+		default:
+			return pm, nil
+		}
+
+	case tea.WindowSizeMsg:
+		pm.progress.Width = msg.Width - padding*2 - 4
+		if pm.progress.Width > maxWidth {
+			pm.progress.Width = maxWidth
+		}
+		return pm, nil
+
+	case tickMsg:
+		pm.percent += stepSize
+		// this check shouldn't be necessary (we check for time)
+		// if pm.percent > 1.0 {
+		// 	pm.percent = 1.0
+		// 	return pm, tea.Quit
+		// }
+
+		t := time.Time(msg)
+		if t.After(pm.timeout) {
+			return pm, tea.Quit
+		}
+		pm.lastTick = t
+
+		return pm, tick()
+
+	default:
+		return pm, nil
+	}
+
+	return pm, nil
+}
+
+func (pm pomoModel) View() string {
+
+	rem := pm.timeout.Sub(pm.lastTick).Round(time.Second)
+	pad := strings.Repeat(" ", padding)
+
+	if pm.percent >= 1.0 {
+		return fmt.Sprintf("\n%s %s %s Complete! 🍅 \n\n", pad, pm.progress.View(pm.percent), pad)
+	}
+
+	return fmt.Sprintf("\n%s %s %s %v \n\n", pad, pm.progress.View(pm.percent), pad, rem)
+}
+
+func main() {
+
+	prog, err := progress.NewModel(progress.WithDefaultScaledGradient(),
+		progress.WithoutPercentage())
+	if err != nil {
+		fmt.Println("Could not initialize progress model:", err)
+		os.Exit(1)
+	}
+
+	pm := pomoModel{
+		timeout:  time.Now().Add(duration),
+		lastTick: time.Now(),
+		progress: prog,
+	}
+
+	if err := tea.NewProgram(pm).Start(); err != nil {
+		fmt.Printf("Couldn't start program: %v\n", err)
+		os.Exit(1)
+	}
+
 }
