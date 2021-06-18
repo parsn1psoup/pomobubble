c8e5f68f60986168d26fe851a18253353a759c5d
Kathrin H <parsn1psoup@users.noreply.github.com>
2021-06-18 21:21:33 +0200

    Introduce breaks

--- go.mod
+++ go.mod
@@ -5,7 +5,7 @@ go 1.16
 require (
 	github.com/charmbracelet/bubbles v0.8.0
 	github.com/charmbracelet/bubbletea v0.14.0
-	github.com/gen2brain/beeep v0.0.0-20210529141713-5586760f0cc1 // indirect
+	github.com/gen2brain/beeep v0.0.0-20210529141713-5586760f0cc1
 	github.com/godbus/dbus/v5 v5.0.4 // indirect
 	github.com/gopherjs/gopherjs v0.0.0-20210614142143-03070f63dd63 // indirect
 	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
--- main.go
+++ main.go
@@ -15,13 +15,14 @@ import (
 )
 
 // TODO
-// add break duration
+// make breaks configurable
+// default value for pomoMinutes instead of placeholder
 // add more key controls (e.g. pause timer)
-// track across multiple pomodoros
 // altscreen?
 // understand implications of WindowSizeMsg better
 // show that progress had started even for long pomodoros (time until first segment of progress bar appears)
 // add tests
+// represent the status with an enum?
 
 // heavily based on
 // https://github.com/charmbracelet/bubbletea/blob/master/examples/countdown/main.go
@@ -29,18 +30,25 @@ import (
 // https://github.com/charmbracelet/bubbletea/blob/master/examples/textinput/main.go
 
 const (
-	fps             = 60
-	padding         = 2
-	maxWidth        = 80
-	refreshInterval = time.Second
+	fps               = 60
+	padding           = 2
+	maxWidth          = 80
+	refreshInterval   = time.Second
+	shortBreakMinutes = 2 // debug
+	longBreakMinutes  = 3 // debug
 )
 
+// maybe move some of these into struct
 var (
-	pomoMinutes   int           = 1
-	pomoDuration  time.Duration = time.Duration(pomoMinutes) * time.Minute
-	inputReceived bool
-	inputComplete bool
-	stepSize      float64
+	pomoMinutes       int
+	inputReceived     bool
+	inputComplete     bool
+	timerRunning      bool
+	stepSize          float64
+	runningPom        bool
+	runningShortBreak bool
+	runningLongBreak  bool
+	awaitingUserInput bool
 )
 
 type pomoModel struct {
@@ -49,6 +57,14 @@ type pomoModel struct {
 	percent   float64
 	progress  *progress.Model
 	textinput textinput.Model // value not pointer, let's see if this makes sense
+	history   pomoHistory
+}
+
+// history of completed pomodoros
+type pomoHistory struct {
+	poms        int
+	shortBreaks int
+	longBreaks  int
 }
 
 // transmits time from ticker to update function
@@ -61,7 +77,9 @@ func tick() tea.Cmd {
 }
 
 func (pm pomoModel) Init() tea.Cmd {
-	fmt.Println("🍅 How long do you want your pomodoros? 🍅\nEnter a number betweeen 1 and 99 for length in minutes, then press Enter to start.")
+	fmt.Println("\n\n🍅 POMOBUBBLE 🍅")
+	// this should be in View() but it always shows up several times there
+	fmt.Println("How long do you want your pomodoros?\nEnter a number betweeen 1 and 99 for length in minutes, then press Enter to start.")
 	return textinput.Blink
 }
 
@@ -77,32 +95,60 @@ func (pm pomoModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		case "ctrl+c", "q", "esc":
 			return pm, tea.Quit
 		case "enter":
-			if !inputReceived {
-				return pm, nil
+			// we are still waiting for the user to specify pomodoro duration
+			if !inputComplete {
+				// if we haven't received any input yet, don't react
+				if !inputReceived {
+					return pm, nil
+				} else {
+					// the user has entered the desired pomodoro duration.
+					// initialize and start the timer
+					min, err := strconv.Atoi(pm.textinput.Value()) // ugly workaround
+					pomoMinutes = min
+					if err != nil {
+						fmt.Printf("Error parsing input: %v\n", err)
+						return pm, tea.Quit
+					}
+					if pomoMinutes < 1 {
+						fmt.Println("Pomodoro duration cannot be less than 1 minute")
+						return pm, tea.Quit
+					}
+					pm.initTimer(pomoMinutes)
+					inputComplete = true
+					timerRunning = true
+					runningPom = true
+					return pm, tick()
+				}
 			} else {
-				pomoMinutes, err := strconv.Atoi(pm.textinput.Value())
-				if err != nil {
-					fmt.Printf("Error parsing input: %v\n", err)
-					return pm, tea.Quit
+				if awaitingUserInput {
+					// find out what's the next interval to start
+					awaitingUserInput = false
+					if runningPom {
+						pm.initTimer(pomoMinutes)
+						timerRunning = true
+						return pm, tick()
+					} else if runningShortBreak {
+						pm.initTimer(shortBreakMinutes)
+						timerRunning = true
+						return pm, tick()
+					} else if runningLongBreak {
+						pm.initTimer(longBreakMinutes)
+						timerRunning = true
+						return pm, tick()
+					}
 				}
-				if pomoMinutes < 1 {
-					fmt.Println("Pomodoro duration cannot be less than 1 minute")
-					return pm, tea.Quit
-				}
-				pomoDuration = time.Duration(pomoMinutes) * time.Minute
-				stepSize = 1.0 / (float64(fps)) / float64(pomoMinutes) // why should stepSize depend on fps? is Update() called on every new frame?
-				pm.timeout = time.Now().Add(pomoDuration)
-				pm.lastTick = time.Now()
-				inputComplete = true
-				return pm, tick()
+				// we weren't expecting the user to provide input
+				return pm, nil
 			}
 
 		default:
-			pm.textinput.Focus()
-			pm.textinput, cmd = pm.textinput.Update(msg)
-			inputReceived = true
-			return pm, cmd
-
+			if !inputComplete {
+				pm.textinput.Focus()
+				pm.textinput, cmd = pm.textinput.Update(msg)
+				inputReceived = true
+				return pm, cmd
+			}
+			return pm, nil
 		}
 
 	case tea.WindowSizeMsg:
@@ -117,8 +163,11 @@ func (pm pomoModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 		t := time.Time(msg)
 		if t.After(pm.timeout) {
-			pm.View() // good for anything?
-			return pm, tea.Quit
+			timerRunning = false
+			pm.updateStatus()
+			notify()
+			awaitingUserInput = true
+			return pm, textinput.Blink
 		}
 		pm.lastTick = t
 
@@ -130,9 +179,59 @@ func (pm pomoModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 }
 
-// View calculates the remaining time (rem) and displays an updated status on progress
+func (pm *pomoModel) initTimer(durationMinutes int) error {
+	pm.percent = 0
+
+	timerDuration := time.Duration(durationMinutes) * time.Minute
+	stepSize = 1.0 / (float64(fps)) / float64(durationMinutes) // why should stepSize depend on fps? is Update() called on every new frame?
+	pm.timeout = time.Now().Add(timerDuration)
+	pm.lastTick = time.Now()
+
+	return nil
+}
+
+// after a time interval is done, update based on what is next
+func (pm *pomoModel) updateStatus() {
+	if runningPom {
+		pm.history.poms++
+		runningPom = false
+		if pm.history.poms%4 == 0 {
+			runningLongBreak = true
+		} else {
+			runningShortBreak = true
+		}
+		return
+	}
+	if runningShortBreak {
+		pm.history.shortBreaks++
+		runningShortBreak = false
+		runningPom = true
+		return
+	}
+
+	if runningLongBreak {
+		pm.history.longBreaks++
+		runningLongBreak = false
+		runningPom = true
+		return
+	}
+
+}
+
+func notify() {
+	if runningPom {
+		beeep.Alert("PomoBubble", "Time to work!", "assets/pom.png")
+	} else if runningShortBreak {
+		beeep.Alert("PomoBubble", "Time for a short break!", "assets/coffee.png")
+	} else if runningLongBreak {
+		beeep.Alert("PomoBubble", "Time for a long break!", "assets/palmtree.png")
+	}
+}
+
+// View renders UI based on model and current status
 func (pm pomoModel) View() string {
 
+	// wait for user to enter valid pomodoro duration
 	if !inputComplete {
 		return fmt.Sprintf(
 			"\n\n%s\n\n%s",
@@ -141,15 +240,24 @@ func (pm pomoModel) View() string {
 		) + "\n"
 	}
 
+	// show progress bar for ongoing pomodoro or break
 	rem := pm.timeout.Sub(pm.lastTick).Round(time.Second)
 	pad := strings.Repeat(" ", padding)
 
-	if pm.percent >= 0.999 {
-		beeep.Alert("PomoBubble", "Time for a break!", "assets/pom.png")
-		return fmt.Sprintf("\n%s %s %s Complete! 🍅 \n\n", pad, pm.progress.View(pm.percent), pad)
+	if timerRunning {
+		if runningPom {
+			return fmt.Sprintf("\nCompleted pomodoros: %v\n\nCurrent pomodoro:\n\n%s %s %s %v \n\n", pm.history.poms, pad, pm.progress.View(pm.percent), pad, rem)
+		} else {
+			return fmt.Sprintf("\nCompleted pomodoros: %v\n\nCurrent break:\n\n%s %s %s %v \n\n", pm.history.poms, pad, pm.progress.View(pm.percent), pad, rem)
+		}
+	} else {
+		// status has already been updated
+		if runningPom {
+			return fmt.Sprintf("\n%s %s %s Complete! 🌴 \n\nPress Enter to start pomodoro.\n\n", pad, pm.progress.View(pm.percent), pad)
+		} else {
+			return fmt.Sprintf("\n%s %s %s Complete! 🍅 \n\nPress Enter to start break.\n\n", pad, pm.progress.View(pm.percent), pad)
+		}
 	}
-
-	return fmt.Sprintf("\n%s %s %s %v \n\n", pad, pm.progress.View(pm.percent), pad, rem)
 }
 
 func main() {
@@ -173,6 +281,7 @@ func main() {
 	pm := pomoModel{
 		progress:  prog,
 		textinput: text,
+		history:   pomoHistory{poms: 0, shortBreaks: 0, longBreaks: 0},
 	}
 
 	if err := tea.NewProgram(pm).Start(); err != nil {
